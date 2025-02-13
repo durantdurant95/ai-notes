ddfb0257fa608d61a5c3369b6ee69e683c40730d
Alejandro Pérez Durán <apduran95@gmail.com>
2025-02-13 14:49:59 -0500

    feat: update Header component styling and enhance global CSS variables for improved theming

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,15 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="p-4 shadow-md">
-      <Link href="/">
+      <Link href="/" className="flex gap-1">
         <Image
           src="/logo.svg"
           alt="Notes.ai Logo"
-          height={30}
-          width={30}
+          height={36}
+          width={36}
           priority
         />
+        <h1 className="font-bold text-primary text-xl">notes.ai</h1>
       </Link>
     </header>
   );
